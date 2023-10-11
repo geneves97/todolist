9cb93a592a2ab0adbe8a9dc94b4bf948fbb53675
George <georgeneves.dev@gmail.com>
2023-10-11 10:51:26 -0300

    feat: adicionando criptografia a senha.

--- src/main/java/br/com/georgeneves/todolist/user/UserController.java
+++ src/main/java/br/com/georgeneves/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.georgeneves.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,8 +25,11 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
 
-        var userCreated = this.userRepository.save(userModel);
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
 
+        userModel.setPassword(passwordHashred);
+
+        var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
