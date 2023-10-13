218a12c066627caa96282b730ddd6ed9ba35d70a
George <georgeneves.dev@gmail.com>
2023-10-13 14:46:36 -0300

    feat1: valida se a tarefa está vinculada ao usuário que está tentando realizar o update

--- src/main/java/br/com/georgeneves/todolist/task/TaskController.java
+++ src/main/java/br/com/georgeneves/todolist/task/TaskController.java
@@ -46,13 +46,20 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+        var task = this.taskRepository.findById(id).orElse(null);
+
         var idUser = request.getAttribute("idUser");
 
-        var task = this.taskRepository.findById(id).orElse(null);
+        if(!task.getIdUser().equals(idUser)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Usuário não tem permissão para alterar esta tarefa");
+        }
 
         Utils.copyNonNullProperties(taskModel, task);
 
-        return this.taskRepository.save(task);
+        var taskUpdated = this.taskRepository.save(task);
+
+        return ResponseEntity.ok().body(this.taskRepository.save(taskUpdated));
     }
 }
