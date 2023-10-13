47c3c2c7543ed6f01fc7e934b78aa780c5bbf6c6
George <georgeneves.dev@gmail.com>
2023-10-13 14:47:11 -0300

    feat1: valida se a tarefa existe

--- src/main/java/br/com/georgeneves/todolist/task/TaskController.java
+++ src/main/java/br/com/georgeneves/todolist/task/TaskController.java
@@ -49,6 +49,11 @@ public class TaskController {
     public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
         var task = this.taskRepository.findById(id).orElse(null);
 
+        if(task == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Tarefa não encontrada");
+        }
+
         var idUser = request.getAttribute("idUser");
 
         if(!task.getIdUser().equals(idUser)){
