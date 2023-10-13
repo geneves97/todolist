011b7837e8820439ab82d2793fe52f753b52b0fa
George <georgeneves.dev@gmail.com>
2023-10-13 19:02:08 -0300

    refactor: corrigindo salvamento duplicado

--- src/main/java/br/com/georgeneves/todolist/task/TaskController.java
+++ src/main/java/br/com/georgeneves/todolist/task/TaskController.java
@@ -65,6 +65,6 @@ public class TaskController {
 
         var taskUpdated = this.taskRepository.save(task);
 
-        return ResponseEntity.ok().body(this.taskRepository.save(taskUpdated));
+        return ResponseEntity.ok().body(taskUpdated);
     }
 }
